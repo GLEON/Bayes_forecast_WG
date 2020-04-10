35341522762f955da4113dab583668c47d7135ce
melofton <melofton@vt.edu>
2020-04-10 13:07:53 -0400

    testing cleaned uncertainty partitioning code on gloeo-only models

--- 4.4_Uncertainty_partitioning/4.4_Uncertainty_partitioning.R
+++ 4.4_Uncertainty_partitioning/4.4_Uncertainty_partitioning.R
@@ -12,131 +12,80 @@
 #load packages
 pacman::p_load(tidyverse)
 
+#setting up counters and vectors for for-loop
 model_names <- c("RW","RW_obs","AR")
+yrs <- c(2015:2016)
+wks <- c(1:20)
+forecast_weeks <- c(1:4)
+N_weeks <- matrix(NA,2,20)
+N_weeks[1,] <- c(1:20)
+N_weeks[2,] <- c(21:40)
 
-forecast_weeks = c(1:4)
+###########################UNCERTAINTY PARTITIONING###################################
 
-for (j in 1:length(model_names)){
-  my_directory <- "C:/Users/Mary Lofton/Dropbox/Ch5/Final_analysis/Data_assim"
-  site = "Midge"
-  N_weeks <- c(1:40)
+for (i in 1:length(model_names)){
 
-  vardat.IC <- matrix(NA, 5000, 40)
-  vardat.IC <- data.frame(vardat.IC)
+  for (n in 1:length(forecast_weeks)){
 
+    #create empty matrices to populate from hindcast variance for each week
+    vardat.det <- matrix(NA, 5000, 40)
+    vardat.det <- data.frame(vardat.det)
 
-  vardat.IC.P <- matrix(NA, 5000, 40)
-  vardat.IC.P <- data.frame(vardat.IC.P)
+    vardat.IC <- matrix(NA, 5000, 40)
+    vardat.IC <- data.frame(vardat.IC)
 
+    vardat.IC.P <- matrix(NA, 5000, 40)
+    vardat.IC.P <- data.frame(vardat.IC.P)
 
-  vardat.IC.P.O <- matrix(NA, 5000, 40)
-  vardat.IC.P.O <- data.frame(vardat.IC.P.O)
+    vardat.IC.P.Pa <- matrix(NA, 5000, 40)
+    vardat.IC.P.Pa <- data.frame(vardat.IC.P.Pa)
 
-  vardat.IC.P.O.Pa <- matrix(NA, 5000, 40)
-  vardat.IC.P.O.Pa <- data.frame(vardat.IC.P.O.Pa)
+    vardat.IC.P.Pa.D <- matrix(NA, 5000, 40)
+    vardat.IC.P.Pa.D <- data.frame(vardat.IC.P.Pa.D)
 
+    for (j in 1:length(yrs)){
 
-  vardat.IC.P.O.Pa.D <- matrix(NA, 5000, 40)
-  vardat.IC.P.O.Pa.D <- data.frame(vardat.IC.P.O.Pa.D)
+      for (k in 1:length(wks)){
 
+      #read in hindcast files from each week and append to matrix
+      dat <- read_csv(file=file.path(paste("./5_Model_output/5.2_Hindcasting/",paste0(model_names[i],'_det.prediction_',yrs[j],'_',wks[k],'.csv'))))
+      vardat.det[,N_weeks[j,k]] <- dat[,forecast_weeks[n]]
 
-  for (i in 1:40){
+      dat <- read_csv(file=file.path(paste("./5_Model_output/5.2_Hindcasting/",paste0(model_names[i],'_hindcast.IC_',yrs[j],'_',wks[k],'.csv'))))
+      vardat.IC[,N_weeks[j,k]] <- dat[,forecast_weeks[n]]
 
-    dat <- read_csv(file=file.path(my_directory,paste(site,paste0(model_names[j],'_forecast.IC_',N_weeks[i],'.csv'),sep = '_')))
-    vardat.IC[,N_weeks[i]] <- dat[,forecast_week]
+      dat <- read_csv(file=file.path(paste("./5_Model_output/5.2_Hindcasting/",paste0(model_names[i],'_hindcast.IC.P_',yrs[j],'_',wks[k],'.csv'))))
+      vardat.IC.P[,N_weeks[j,k]] <- dat[,forecast_weeks[n]]
 
-    dat <- read_csv(file=file.path(my_directory,paste(site,paste0(model_names[j],'_forecast.IC.P_',N_weeks[i],'.csv'),sep = '_')))
-    vardat.IC.P[,i] <- dat[,forecast_week]
+      if(!model_names[i] %in% c("RW","RW_obs")){
+        dat <- read_csv(file=file.path(paste("./5_Model_output/5.2_Hindcasting/",paste0(model_names[i],'_hindcast.IC.P.Pa_',yrs[j],'_',wks[k],'.csv'))))
+        vardat.IC.P.Pa[,N_weeks[j,k]] <- dat[,forecast_weeks[n]]}
 
-    dat <- read_csv(file=file.path(my_directory,paste(site,paste0(model_names[j],'_forecast.IC.P.O_',N_weeks[i],'.csv'),sep = '_')))
-    vardat.IC.P.O[,i] <- dat[,forecast_week]
+      if(!model_names[i] %in% c("RW","RW_obs","AR")){
+        dat <- read_csv(file=file.path(paste("./5_Model_output/5.2_Hindcasting/",paste0(model_names[i],'_hindcast.IC.P.Pa.D_',yrs[j],'_',wks[k],'.csv'))))
+        vardat.IC.P.Pa.D[,N_weeks[j,k]] <- dat[,forecast_weeks[n]]}
 
-    if(!model_names[j] %in% c("Seasonal_RandomWalk","Seasonal_RandomWalk_Obs_error")){
-      dat <- read_csv(file=file.path(my_directory,paste(site,paste0(model_names[j],'_forecast.IC.P.O.Pa_',N_weeks[i],'.csv'),sep = '_')))
-      vardat.IC.P.O.Pa[,i] <- dat[,forecast_week]}
+      }}
 
-    if(!model_names[j] %in% c("Seasonal_RandomWalk","Seasonal_RandomWalk_Obs_error","Seasonal_AR")){
-      dat <- read_csv(file=file.path(my_directory,paste(site,paste0(model_names[j],'_forecast.IC.P.O.Pa.D_',N_weeks[i],'.csv'),sep = '_')))
-      vardat.IC.P.O.Pa.D[,i] <- dat[,forecast_week]}
+    #write variance matrices to .csv
+    write.csv(vardat.det,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_vardat.det_',forecast_weeks[n],'.csv'))),row.names = FALSE)
+    write.csv(vardat.IC,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_vardat.IC_',forecast_weeks[n],'.csv'))),row.names = FALSE)
+    write.csv(vardat.IC.P,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_vardat.IC.P_',forecast_weeks[n],'.csv'))),row.names = FALSE)
+    if(!model_names[i] %in% c("RW","RW_obs")){
+    write.csv(vardat.IC.P.Pa,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_vardat.IC.P.Pa_',forecast_weeks[n],'.csv'))),row.names = FALSE)}
+    if(!model_names[i] %in% c("RW","RW_obs","AR")){
+    write.csv(vardat.IC.P.Pa.D,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_vardat.IC.P.Pa.D_',forecast_weeks[n],'.csv'))),row.names = FALSE)}
+
+    #calculate relative uncertainty contributions and write to file
+    source('0_Function_library/uncertainty_partitioning_make_varmat.R')
+
+    varMat   <- make_varMat(model_name = model_names[i])
+    write.csv(varMat,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_varMat_',forecast_weeks[n],'.csv'))),row.names = FALSE)
+
+    varRelative <- apply(varMat,2,function(x) {x/max(x)})
+    write.csv(varRelative,file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_varRelative_',forecast_weeks[n],'.csv'))),row.names = FALSE)
 
   }
-
-  vardat.IC <- vardat.IC[,c(1:16,21:36)]
-  vardat.IC.P <- vardat.IC.P[,c(1:16,21:36)]
-  vardat.IC.P.O <- vardat.IC.P.O[,c(1:16,21:36)]
-  vardat.IC.P.O.Pa <- vardat.IC.P.O.Pa[,c(1:16,21:36)]
-  vardat.IC.P.O.Pa.D <- vardat.IC.P.O.Pa.D[,c(1:16,21:36)]
-
-
-
-  ### calculation of variances
-  source('RCode/Helper_functions/forecast_plug_n_play_data_assim.R')
-
-  varMat   <- make_varMat(model_name = model_names[j])
-  rowMeans(varMat, na.rm = TRUE)
-  write.csv(varMat, file=file.path("Results/Uncertainty_partitioning",paste(site,paste0(model_names[j],'_total_var.csv'), sep = '_')),row.names = FALSE)
-
-  ###consider adding code here to make sure the intervals are ordered from smallest to greatest
-  #to avoid weird overlapping when plotting due to small decreases in predictions
-  #with all uncertainties incorporated due to chance
-  V.pred.rel.2015 <- apply(varMat[,1:16],2,function(x) {x/max(x)})
-  V.pred.rel.2016 <- apply(varMat[,17:32],2,function(x) {x/max(x)})
-  write.csv(V.pred.rel.2015,file=file.path("Results/Uncertainty_partitioning",paste(site,paste0(model_names[j],'_varMat_2015.csv'), sep = '_')),row.names = FALSE)
-  write.csv(V.pred.rel.2016,file=file.path("Results/Uncertainty_partitioning",paste(site,paste0(model_names[j],'_varMat_2016.csv'), sep = '_')),row.names = FALSE)
-
-
-  # #plot variances
-  # dev.off(dev.list()["RStudioGD"])
-  # plot_varMat(model_name = model_name)
-  #
-  # ## write stacked area plot to file
-  # png(file=file.path(my_directory,paste(site,paste0(model_name,'_var_part.png'), sep = '_')), res=300, width=30, height=10, units='cm')
-  # plot_varMat(model_name = model_name)
-  # dev.off()
-  #
-  #
-  # ##looking at percentile of obs in forecast distribution
-  forecast_y <- log(as.matrix(read_csv("./Datasets/Sunapee/SummarizedData/Midge_year_by_week_totalperL_forecast_05OCT19.csv"))+0.003)
-  forecast_y <- exp(forecast_y[7:8,])
-  forecast_ys <- forecast_y[,-c(17:20)]
-
-  obs_quantile <- NULL
-  if(!model_names[j] %in% c("Seasonal_RandomWalk","Seasonal_RandomWalk_Obs_error")){
-    for (i in 1:length(forecast_ys)){
-      percentile1 <- ecdf(exp(vardat.IC.P.O.Pa[,i])) ##be sure to change this as needed - needs to be made into a function!!!
-      obs_quantile[i] <- percentile1(forecast_ys[i])
-    }} else if(!model_names[j] %in% c("Seasonal_RandomWalk","Seasonal_RandomWalk_Obs_error","Seasonal_AR")){
-      for (i in 1:length(forecast_ys)){
-        percentile1 <- ecdf(exp(vardat.IC.P.O.Pa.D[,i])) ##be sure to change this as needed - needs to be made into a function!!!
-        obs_quantile[i] <- percentile1(forecast_ys[i])
-      }} else{
-        for (i in 1:length(forecast_ys)){
-          percentile1 <- ecdf(exp(vardat.IC.P.O[,i])) ##be sure to change this as needed - needs to be made into a function!!!
-          obs_quantile[i] <- percentile1(forecast_ys[i])
-        }}
-
-
-  #should add vertical line at 0.5 to this
-  png(file=file.path(my_directory,paste(site,paste0(model_names[j],'_obs_decile_4wk.png'), sep = '_')), res=300, width=10, height=10, units='cm')
-  hist(obs_quantile,xlab = "Quantile of obs. in forecast interval", main = "",
-       cex.axis = 1.2, cex.lab = 1.2, xlim = c(0,1), breaks = seq(0,1,0.1))
-  dev.off()
-
-  png(file=file.path(my_directory,paste(site,paste0(model_names[j],'_obs_quartile_4wk.png'), sep = '_')), res=300, width=10, height=10, units='cm')
-  hist(obs_quantile,xlab = "Quantile of obs. in forecast interval", main = "",
-       cex.axis = 1.2, cex.lab = 1.2, xlim = c(0,1), breaks = seq(0,1,0.25))
-  dev.off()
 }
-# #a perfect forecast
-# obs_quantile <- rep(0.5, 40)
-# png(file=file.path(my_directory,paste("perfect_forecast.png")), res=300, width=10, height=10, units='cm')
-# hist(obs_quantile,xlab = "Quantile of obs. in forecast interval", main = "",
-#      cex.axis = 1.2, cex.lab = 1.2, breaks = seq(0,1,0.1))
-# dev.off()
-#
-# #an extremely good forecast
-# obs_quantile <- c(0.2, 0.3, 0.3, 0.4, 0.4, 0.4, 0.4, rep(0.5,26), 0.6, 0.6, 0.6, 0.6, 0.7, 0.7, 0.8)
-# png(file=file.path(my_directory,paste("excellent_forecast.png")), res=300, width=10, height=10, units='cm')
-# hist(obs_quantile,xlab = "Quantile of obs. in forecast interval", main = "",
-#      cex.axis = 1.2, cex.lab = 1.2, breaks = seq(0,1,0.1))
-# dev.off()
+
+##Hokie hokie hokie hi! You have partitioned uncertainty. Give someone a hug :-)
