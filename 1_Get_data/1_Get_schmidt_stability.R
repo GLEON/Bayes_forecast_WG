d4c439766d41d80a869d312acc2a1ac02f6a27e2
jabrent <jennie.brentrup@gmail.com>
2020-04-10 16:11:52 -0400

    Adding scripts for organizing and cleaning Onset water temp and schmidt stability

---
+++ 1_Get_data/1_Get_schmidt_stability.R
@@ -0,0 +1,369 @@
+# Script to download and wrangle Sunapee GLEON buoy water temp string thermistor data from EDI
+# Last updated 2020 April 9 - JB
+
+# Load packages ####
+# run this line if you do not have pacman installed
+#install.packages('pacman')
+
+#load other packages
+pacman::p_load(tidyverse, lubridate, googledrive, rLakeAnalyzer, openair)
+
+# Download data ####
+#download data file into appropriate local folder
+
+# Sunapee GLEON buoy water temp thermistor data
+my_url <- "https://drive.google.com/file/d/1mN_GNVdiiAHLzoQCRS7Vh9amjSrgJKD9/view?usp=sharing"
+
+drive_download(
+  file = drive_get(my_url),
+  path = "./00_Data_files/EDI_data_clones/2007-e2019_buoy_tempstring_v07April2020.csv", overwrite = TRUE)
+
+# Alternative way to get file ID
+as_id(drive_find(pattern = "buoy.tempstring/2007-e2019_buoy_tempstring_v07April2020.csv")$id)
+
+drive_download(file = as_id("1mN_GNVdiiAHLzoQCRS7Vh9amjSrgJKD9"),
+               path = "./00_Data_files/EDI_data_clones/2007-e2019_buoy_tempstring_v07April2020.csv", overwrite = TRUE)
+
+# 2015 HOBO temp string data
+drive_download(
+  file = as_id(drive_find(pattern = "buoy.tempstring/2015_hobotempstring_L1.csv")$id),
+  path = "./00_Data_files/EDI_data_clones/2015_hobotempstring_L1.csv", overwrite = TRUE)
+
+# Alternative way to get file ID
+as_id(drive_find(pattern = "buoy.tempstring/2015_hobotempstring_L1.csv")$id)
+
+drive_download(file = as_id("1OqPKgnx5Hk8TwnuM9r64g414ENj6YQsP"),
+               path = "./00_Data_files/EDI_data_clones/2015_hobotempstring_L1.csv.csv", overwrite = TRUE)
+
+# Load buoy data into R ####
+buoy <- read_csv("./00_Data_files/EDI_data_clones/2007-e2019_buoy_tempstring_v07April2020.csv",
+                 col_types = list(
+                   datetime = col_datetime(format = ""),
+                   location = col_character(),
+                   TempC_0p5m = col_double(),
+                   TempC_0p75m = col_double(),
+                   TempC_0p85m = col_double(),
+                   TempC_1m = col_double(),
+                   TempC_1p5m = col_double(),
+                   TempC_1p75m = col_double(),
+                   TempC_1p85m = col_double(),
+                   TempC_2m = col_double(),
+                   TempC_2p5m = col_double(),
+                   TempC_2p75m = col_double(),
+                   TempC_2p85m = col_double(),
+                   TempC_3m = col_double(),
+                   TempC_3p5m = col_double(),
+                   TempC_3p75m = col_double(),
+                   TempC_3p85m = col_double(),
+                   TempC_4p5m = col_double(),
+                   TempC_4p75m = col_double(),
+                   TempC_4p85m = col_double(),
+                   TempC_5p5m = col_double(),
+                   TempC_5p75m = col_double(),
+                   TempC_5p85m = col_double(),
+                   TempC_6p5m = col_double(),
+                   TempC_6p75m = col_double(),
+                   TempC_6p85m = col_double(),
+                   TempC_7p5m = col_double(),
+                   TempC_7p75m = col_double(),
+                   TempC_7p85m = col_double(),
+                   TempC_8p5m = col_double(),
+                   TempC_8p75m = col_double(),
+                   TempC_8p85m = col_double(),
+                   TempC_9p5m = col_double(),
+                   TempC_9p75m = col_double(),
+                   TempC_9p85m = col_double(),
+                   TempC_10p5m = col_double(),
+                   TempC_11p5m = col_double(),
+                   TempC_13p5m = col_double(),
+                   temp_flag = col_character()
+                 ))
+
+# load HOBO data from 2015 into R ####
+hobo <- read_csv("./00_Data_files/EDI_data_clones/2015_hobotempstring_L1.csv", col_types = list(
+    datetime = col_datetime(format = ""),
+    TempC_0p5m = col_double(),
+    TempC_1p5m = col_double(), #col_logical(), no data just NA
+    TempC_2p5m = col_double(),
+    TempC_3p5m = col_double(),
+    TempC_4p5m = col_double(),
+    TempC_5p5m = col_double(),
+    TempC_6p5m = col_double(),
+    TempC_7p5m = col_double(),
+    TempC_8p5m = col_double()
+  )
+)
+
+# Water temp data cleaning for buoy & hobo data ####
+
+# BUOY
+#eliminate data where bottom water thermistors may have been in sediment
+#get in format for calculating Schmidt stability in rLakeAnalyzer
+buoy1 <- buoy %>%
+  mutate(date = date(datetime)) %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  filter(year %in% 2009:2016 & month %in% 5:10)
+
+#set colnames for rLakeAnalyzer
+colnames(buoy1)[3:38] <- c("wtr_0.5","wtr_0.75","wtr_0.85","wtr_1","wtr_1.5","wtr_1.75",
+                           "wtr_1.85","wtr_2","wtr_2.5","wtr_2.75","wtr_2.85","wtr_3",
+                           "wtr_3.5","wtr_3.75","wtr_3.85","wtr_4.5","wtr_4.75","wtr_4.85",
+                           "wtr_5.5","wtr_5.75","wtr_5.85","wtr_6.5","wtr_6.75","wtr_6.85",
+                           "wtr_7.5","wtr_7.75","wtr_7.85","wtr_8.5","wtr_8.75","wtr_8.85",
+                           "wtr_9.5","wtr_9.75","wtr_9.85","wtr_10.5","wtr_11.5","wtr_13.5")
+
+
+# To see data flags
+print(unique(buoy1$temp_flag)) #  "11.5b, 13.5b" "i", "10.5dn"
+
+buoy2 <- buoy1 %>%
+  select(-ends_with(c(".75", ".85"))) %>%  # remove extra depths for data not part of time series
+  mutate(wtr_10.5 = ifelse(temp_flag == "10.5dn",NA,wtr_10.5),
+        wtr_11.5 = ifelse(temp_flag == "11.5b, 13.5b",NA,wtr_11.5),
+         wtr_13.5 = ifelse(temp_flag == "11.5b, 13.5b",NA,wtr_13.5)) %>%
+  filter(year!=2015) %>%    # no buoy data, use HOBO temp string data instead
+  select(datetime,wtr_0.5:wtr_10.5) # remove 11.5 & 13.5 m - no data
+
+
+# HOBO
+
+#set colnames for rLakeAnalyzer
+colnames(hobo)[2:10] <- c("wtr_0.5","wtr_1.5","wtr_2.5",
+                           "wtr_3.5","wtr_4.5",
+                           "wtr_5.5","wtr_6.5",
+                           "wtr_7.5","wtr_8.5")
+
+
+# Calculate Schimdt stability ####
+
+# Load bathymetric data
+bathy <- load.bathy("./00_Data_files/Sunapee.bth")
+
+schmidt_buoy <- ts.schmidt.stability(buoy2, bathy, na.rm = TRUE)
+
+schmidt_hobo <- ts.schmidt.stability(hobo, bathy, na.rm = TRUE)
+
+# Schmidt Data cleaning  ####
+
+# set negative values to 0 or remove if too early in year - cite Bruesewitz et al. 2015
+plot(schmidt.stability ~ datetime, data = schmidt_buoy)
+points(schmidt.stability ~ datetime, data = schmidt_hobo, col = 'red')
+
+# 2009 Cleaning
+# Remove values < 0 and < 50 before Sep 14
+schmidt_buoy_2009_part1 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2009) %>%
+  filter(date < "2009-09-14") %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,NA, schmidt.stability)) %>% # remove negative values
+  filter(schmidt.stability > 50) # remove outliers in Aug/Sep
+
+schmidt_buoy_2009_part2 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2009) %>%
+  filter(date >= "2009-09-14") %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,0, schmidt.stability)) # set negative values to 0
+
+# Join all 2009 data back together
+schmidt_buoy_2009 <- bind_rows(schmidt_buoy_2009_part1, schmidt_buoy_2009_part2) %>%
+  arrange(datetime)
+
+#2010 Cleaning
+schmidt_buoy_2010_part1 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2010) %>%
+  filter(date <= "2010-06-14")
+
+schmidt_buoy_2010_part2 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2010) %>%
+  filter(date > "2010-06-14") %>%
+  filter(date < "2010-07-05") %>%
+  filter(schmidt.stability > 50)
+
+schmidt_buoy_2010_part3 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2010) %>%
+  filter(date >= "2010-07-05") %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,0, schmidt.stability)) # set negative values to 0
+
+# Join all 2010 data back together
+schmidt_buoy_2010 <- bind_rows(schmidt_buoy_2010_part1, schmidt_buoy_2010_part2, schmidt_buoy_2010_part3) %>%
+  arrange(datetime)
+
+# 2011 & 2012, 2016 Cleaning
+schmidt_buoy_2011_2012_2016 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year %in% c(2011,2012,2016)) %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,0, schmidt.stability)) # set negative values to 0
+
+# 2013 Cleaning
+schmidt_buoy_2013_part1 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2013) %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,0, schmidt.stability)) # set negative values to 0
+
+# Remove low outliers in August 2013-08-12 16:40:00, 2013-08-12 16:50:00
+schmidt_buoy_2013 <- schmidt_buoy_2013_part1[-c(14933:14934),]
+
+# 2014 Cleaning
+schmidt_buoy_2014_part1 <- schmidt_buoy %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  filter(year == 2014)
+
+# Remove low outliers in August 2014-08-13 16:40:00 - 2014-08-13 17:10:00
+schmidt_buoy_2014_part2 <- schmidt_buoy_2014_part1[-c(15077:15080),]
+
+schmidt_buoy_2014 <- schmidt_buoy_2014_part2 %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,0, schmidt.stability)) # set negative values to 0
+
+#2015 Cleaning
+schmidt_hobo_2015 <- schmidt_hobo %>%
+  mutate(year = year(datetime)) %>%
+  mutate(month = month(datetime)) %>%
+  mutate(date = date(datetime)) %>%
+  mutate(schmidt.stability = ifelse(schmidt.stability < 0,0, schmidt.stability)) # set negative values to 0
+
+# Bind all rows for schmidt togeher
+
+schmidt_buoy1 <- bind_rows(schmidt_buoy_2009, schmidt_buoy_2010, schmidt_buoy_2011_2012_2016, schmidt_buoy_2013, schmidt_buoy_2014,schmidt_hobo_2015) %>%
+  arrange(datetime) %>%
+  select(datetime, year, schmidt.stability)
+
+# Summarize schmidt for daily summary ####
+
+# Add full time series to data to be able to calculate daily averages but cut-off days with schmidt data for only part of day
+full_datetime <- seq(from = ymd_hms("2009-05-21 00:00:00"), to = ymd_hms("2016-10-05 23:00:00"), by = dminutes(10))
+full_datetime.df <- as_tibble(full_datetime) %>%
+  mutate(year = year(value))
+colnames(full_datetime.df) <- c("datetime", "year")
+
+# Separate time series for 2015 since data were 15 minute intervals instead of 10
+full_datetime_2015 <- seq(from = ymd_hms("2015-05-21 00:00:00"), to = ymd_hms("2015-10-02 23:00:00"), by = dminutes(15))
+full_datetime_2015.df <- as_tibble(full_datetime_2015)
+colnames(full_datetime_2015.df) <- "datetime"
+
+
+# Join all years of time series except 2015 with schmidt stability data since 2015 data collected every 15 min
+schmidt_buoy_no2015 <- schmidt_buoy1 %>%
+  filter(year != 2015) %>%
+  select(-year)
+
+full_datetime.df_no2015 <- full_datetime.df %>%
+  filter(year != 2015) %>%
+  select(-year)
+
+schmidt_buoy2 <- left_join(full_datetime.df_no2015, schmidt_buoy_no2015, by = "datetime") %>%
+  rename(date = datetime) # open air function needs date column so renamed datetime
+
+# 2015 join
+schmidt_buoy_2015 <- schmidt_buoy1 %>%
+  filter(year == 2015) %>%
+  select(-year)
+
+schmidt_buoy2015_fulltime <- left_join(full_datetime_2015.df, schmidt_buoy_2015, by = "datetime") %>%
+  rename(date = datetime) # open air function needs date column so renamed datetime
+
+# Set threshold to 75% of hourly data needs to be present to calculate the daily summary
+
+# all years except 2015
+schmidt_daily_mean <- timeAverage(schmidt_buoy2, avg.time = "day", data.thresh = 75, statistic = "mean", start.date = "2009-05-21 00:00:00", end.date = "2016-10-05 23:00:00", interval = "10 min")
+
+schmidt_daily_median <- timeAverage(schmidt_buoy2, avg.time = "day", data.thresh = 75, statistic = "median", start.date = "2009-05-21 00:00:00", end.date = "2016-10-05 23:00:00", interval = "10 min")
+
+schmidt_daily_min <- timeAverage(schmidt_buoy2, avg.time = "day", data.thresh = 75, statistic = "min", start.date = "2009-05-21 00:00:00", end.date = "2016-10-05 23:00:00", interval = "10 min")
+
+schmidt_daily_max <- timeAverage(schmidt_buoy2, avg.time = "day", data.thresh = 75, statistic = "max", start.date = "2009-05-21 00:00:00", end.date = "2016-10-05 23:00:00", interval = "10 min")
+
+schmidt_daily_sd <- timeAverage(schmidt_buoy2, avg.time = "day", data.thresh = 75, statistic = "sd", start.date = "2009-05-21 00:00:00", end.date = "2016-10-05 23:00:00", interval = "10 min")
+
+# just 2015
+schmidt_daily_mean_2015 <- timeAverage(schmidt_buoy2015_fulltime, avg.time = "day", data.thresh = 75, statistic = "mean", start.date = "2015-05-21 00:00:00", end.date = "2015-10-02 23:00:00", interval = "15 min")
+
+schmidt_daily_median_2015 <- timeAverage(schmidt_buoy2015_fulltime, avg.time = "day", data.thresh = 75, statistic = "median", start.date = "2015-05-21 00:00:00", end.date = "2015-10-02 23:00:00", interval = "15 min")
+
+schmidt_daily_min_2015 <- timeAverage(schmidt_buoy2015_fulltime, avg.time = "day", data.thresh = 75, statistic = "min", start.date = "2015-05-21 00:00:00", end.date = "2015-10-02 23:00:00", interval = "15 min")
+
+schmidt_daily_max_2015 <- timeAverage(schmidt_buoy2015_fulltime, avg.time = "day", data.thresh = 75, statistic = "max", start.date = "2015-05-21 00:00:00", end.date = "2015-10-02 23:00:00", interval = "15 min")
+
+schmidt_daily_sd_2015 <- timeAverage(schmidt_buoy2015_fulltime, avg.time = "day", data.thresh = 75, statistic = "sd", start.date = "2015-05-21 00:00:00", end.date = "2015-10-02 23:00:00", interval = "15 min")
+
+#Bind summaries together
+schmidt_daily_summary1 <- bind_cols(schmidt_daily_mean, schmidt_daily_median[,2], schmidt_daily_min[,2], schmidt_daily_max[,2], schmidt_daily_sd[,2])
+
+# rename columns
+schmidt_daily_summary2 <- schmidt_daily_summary1 %>%
+  rename(schmidt.stability_mean = schmidt.stability, schmidt.stability_median = schmidt.stability1, schmidt.stability_min = schmidt.stability2, schmidt.stability_max = schmidt.stability3,schmidt.stability_sd = schmidt.stability4) %>%
+  mutate(date = date(date)) %>%
+  mutate(year = year(date)) %>%
+  filter(year !=2015) # remove 2015 to be able to bind separate data
+
+#Bind 2015 summaries together
+schmidt_daily_summary2015 <- bind_cols(schmidt_daily_mean_2015, schmidt_daily_median_2015[,2], schmidt_daily_min_2015[,2], schmidt_daily_max_2015[,2], schmidt_daily_sd_2015[,2])
+
+# rename columns
+schmidt_daily_summary2015_2 <- schmidt_daily_summary2015 %>%
+  rename(schmidt.stability_mean = schmidt.stability, schmidt.stability_median = schmidt.stability1, schmidt.stability_min = schmidt.stability2, schmidt.stability_max = schmidt.stability3,schmidt.stability_sd = schmidt.stability4) %>%
+  mutate(date = date(date)) %>%
+  mutate(year = year(date))
+
+# Bind all schmidt daily summaries
+schmidt_daily_summary3 <- bind_rows(schmidt_daily_summary2, schmidt_daily_summary2015_2) %>%
+  arrange(date) %>%
+  select(-year)
+
+# Check data with plots
+plot(schmidt.stability_mean ~ date, data = schmidt_daily_summary3)
+lines(schmidt.stability_median ~ date, data = schmidt_daily_summary3, col = "blue")
+lines(schmidt.stability_min ~ date, data = schmidt_daily_summary3, col = "gray")
+lines(schmidt.stability_max ~ date, data = schmidt_daily_summary3, col = "red")
+
+
+# Limit schmidt data to sampling dates ####
+
+#read in sampling dates
+sampling_dates <- read_csv("./00_Data_files/Bayesian_model_input_data/sampling_dates.csv")
+
+schmidt_daily_summary4 <- schmidt_daily_summary3 %>%
+  filter(date %in% sampling_dates$date)
+
+# Write csv ####
+write_csv(schmidt_daily_summary4, "./00_Data_files/Covariate_analysis_data/schmidt_stability_daily_summary.csv")
+
+# Add in gap filled data for schmidt values 1-2 days apart from missing data ####
+
+schmidt_daily_summary3_subset <- schmidt_daily_summary3[c(1547, 1848, 1892),]
+
+schmidt_daily_summary3_subset[1,1] <- ymd("2013-08-15") #1 day apart
+schmidt_daily_summary3_subset[2,1] <- ymd("2014-06-12") #1 day apart
+schmidt_daily_summary3_subset[3,1] <- ymd("2014-07-23") #2 days apart
+
+# filter dat5 to remove NA dates
+schmidt_daily_summary5 <- schmidt_daily_summary4 %>%
+  filter(!date %in% schmidt_daily_summary3_subset$date)
+
+# full join for more complete water temp data
+schmidt_daily_summary6 <- full_join(schmidt_daily_summary5, schmidt_daily_summary3_subset) %>%
+  arrange(date)
+
+# Write data for 2nd dataset with water temp holes filled in ####
+write_csv(schmidt_daily_summary6, "./00_Data_files/Covariate_analysis_data/schmidt_stability_daily_summary_gap_filled.csv")
+
+
