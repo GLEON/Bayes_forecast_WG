c587554b233c16242894ce3e7aea85cd251a4e29
jabrent <jennie.brentrup@gmail.com>
2020-04-13 23:19:34 -0400

    Updates to precip data

--- 1_Get_data/1_Get_PRISM_precip.R
+++ 1_Get_data/1_Get_PRISM_precip.R
@@ -46,4 +46,37 @@ precip2 <- precip1 %>%
   select(-c(year, month))
 
 # Write data ####
-write_csv(precip2, "./00_Data_files/Covariate_analysis_data/PRISM_precip_daily_summary.csv")
+write_csv(precip2, "./00_Data_files/Covariate_analysis_data/PRISM_precip_daily_sum.csv")
+
+
+# Create 1 day & 1 week lag precip data ####
+
+# Read in sampling dates lag
+sampling_dates_lag <- read_csv("./00_Data_files/Covariate_analysis_data/sampling_dates_lag.csv")
+
+#Join precip with 1 day lag date
+precip_lag_day <- precip1 %>%
+  rename(date_1daylag = date) %>%
+  select(date_1daylag, precip_mm)
+
+precip_lag_day1 <- left_join(sampling_dates_lag[,c(1,3)], precip_lag_day, by = "date_1daylag") %>%
+  rename(precip_mm_1daylag = precip_mm)
+
+# Write precip 1 day lag data
+write_csv(precip_lag_day1, "./00_Data_files/Covariate_analysis_data/PRISM_precip_daily_sum_1daylag.csv")
+
+#Join precip with 1 week lag date
+precip_lag_week <- precip1 %>%
+  rename(date_1weeklag = date) %>%
+  select(date_1weeklag, precip_mm)
+
+precip_lag_week1 <- left_join(sampling_dates_lag[,1:2], precip_lag_week, by = "date_1weeklag") %>%
+  rename(precip_mm_1weeklag = precip_mm)
+
+# Write precip 1 week lag data
+write_csv(precip_lag_week1, "./00_Data_files/Covariate_analysis_data/PRISM_precip_daily_sum_1weeklag.csv")
+
+# Combine all precip data ###
+precip_all <- bind_cols(precip2,precip_lag_day1[,3], precip_lag_week1[,3])
+
+write_csv(precip_all, "./00_Data_files/Covariate_analysis_data/PRISM_precip_all.csv")
