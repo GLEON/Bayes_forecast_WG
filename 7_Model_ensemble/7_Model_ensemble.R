746d236b3bce8d01fa9ffa2dcf354eed41cc79d7
melofton <melofton@vt.edu>
2020-04-24 15:50:39 -0400

    running model ensemble exercise

--- 7_Model_ensemble/7_Model_ensemble.R
+++ 7_Model_ensemble/7_Model_ensemble.R
@@ -7,9 +7,10 @@
 #2. Append rows of all vardat files and write ensemble vardat to file
 #3. Calculate varmat from appended vardats and write to file
 #4. Calculate relative varmat from varmat and write to file
-#5. Calculate output analysis metrics on appended vardats
-#6. Calculate uncertainty partitioning analysis metrics from relative varmat
-#7. Repeat steps 1-6 for each forecast week
+#5. Calculate ensemble PI
+#6. Calculate output analysis metrics on ensemble PI
+#7. Calculate uncertainty partitioning analysis metrics from relative varmat
+#8. Repeat steps 1-7 for each forecast week
 ################################################################################
 
 
@@ -29,13 +30,13 @@ hindcast_types <- c("IC","IC.Pa","IC.Pa.D","IC.Pa.D.P")
 
 #1. Read in all vardat files for all models EXCEPT RW MODELS for each type of hindcast (IC, IC.P, etc.)
 #2. Append rows of all vardat files and write ensemble vardat to file
-for (n in 1:length(forecast_weeks)){
+for (n in 4:length(forecast_weeks)){
 
   for (j in 1:length(hindcast_types)){
 
     ensemble_vardat <- read_csv(file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[1],'_vardat.',hindcast_types[j],'_',forecast_weeks[n],'.csv'))))
 
-  for (i in 1:length(model_names)){
+  for (i in 2:length(model_names)){
 
     #read in vardats for various hindcast types for each model
     vardat <- read_csv(file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0(model_names[i],'_vardat.',hindcast_types[j],'_',forecast_weeks[n],'.csv'))))
@@ -55,6 +56,7 @@ for (n in 1:length(forecast_weeks)){
       AR_vardat <- read_csv(file=file.path(paste("./5_Model_output/5.3_Uncertainty_partitioning/",paste0('AR_vardat.IC.Pa.P_',forecast_weeks[n],'.csv'))))
       ensemble_vardat <- rbind(ensemble_vardat, AR_vardat)
     }
+
     write.csv(ensemble_vardat,file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.',hindcast_types[j],'_',forecast_weeks[n],'.csv'))),row.names = FALSE)
 
   }
@@ -71,9 +73,9 @@ forecast_weeks <- c(1:4)
 for (n in 1:length(forecast_weeks)){
 
   vardat.IC <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC_',forecast_weeks[n],'.csv')))))
-  vardat.IC.P <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.P_',forecast_weeks[n],'.csv')))))
-  vardat.IC.P.Pa <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.P.Pa_',forecast_weeks[n],'.csv')))))
-  vardat.IC.P.Pa.D <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.P.Pa.D_',forecast_weeks[n],'.csv')))))
+  vardat.IC.Pa <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.Pa_',forecast_weeks[n],'.csv')))))
+  vardat.IC.Pa.D <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.Pa.D_',forecast_weeks[n],'.csv')))))
+  vardat.IC.Pa.D.P <- vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.Pa.D.P_',forecast_weeks[n],'.csv')))))
 
   #calculate relative uncertainty contributions and write to file
   source('0_Function_library/uncertainty_partitioning_make_varmat.R')
@@ -88,12 +90,38 @@ for (n in 1:length(forecast_weeks)){
 
 rm(list = ls())
 
-#5. Calculate output analysis metrics on appended vardats
+#5. Calculate ensemble PI
 
-########################CALCULATE ASSESSMENT METRICS#####################################
-model_name = "ensemble"
+#setting up counters and vectors for for-loop
+model_names <- c("wtrtemp_min","wtrtemp_min_lag","wtrtemp_MA7","schmidt_med_diff","wnd_dir_2day_lag","GDD","schmidt_max_lag","precip","schmidt_and_wnd","schmidt_and_precip","wnd_and_precip","wnd_and_GDD")
 forecast_weeks <- c(1:4)
 
+for (n in 4:length(forecast_weeks)){
+
+  ensemble_PI <- read_csv(file=file.path(paste("./6_Output_analysis/6.1_Predictive_intervals/",paste0(model_names[1],'_PI_',forecast_weeks[n],'.csv'))))
+
+    for (i in 2:length(model_names)){
+
+      #read in PIs for each model
+      PI <- read_csv(file=file.path(paste("./6_Output_analysis/6.1_Predictive_intervals/",paste0(model_names[i],'_PI_',forecast_weeks[n],'.csv'))))
+      ensemble_PI <- rbind(ensemble_PI, PI)
+    }
+
+    write.csv(ensemble_PI,file=file.path(paste("./7_Model_ensemble/",paste0('ensemble_PI_',forecast_weeks[n],'.csv'))),row.names = FALSE)
+
+}
+
+rm(list = ls())
+
+
+#6. Calculate output analysis metrics on ensemble PI
+
+########################CALCULATE ASSESSMENT METRICS#####################################
+#setting up counters and vectors for for-loop
+model_name <- "ensemble"
+forecast_weeks <- c(1:4)
+
+########################CALCULATE ASSESSMENT METRICS#####################################
 for (n in 1:length(forecast_weeks)){
 
   #read in observed data
@@ -126,21 +154,17 @@ for (n in 1:length(forecast_weeks)){
     source('0_Function_library/output_analysis_assessment_metrics.R')
 
     #read in appropriate hindcast summary files
-    vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0('ensemble.vardat.IC.P.Pa.D_',forecast_weeks[n],'.csv')))))
-    varMat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0(model_name,'_varMat_',forecast_weeks[n],'.csv')))))
+    vardat <- as.matrix(read_csv(file=file.path(paste("./7_Model_ensemble/",paste0(model_name,'_PI_',forecast_weeks[n],'.csv')))))
 
     #subset vardat and varMat according to forecast week
     if(forecast_weeks[n] == 4){
       vardat <- vardat[,c(1:17,21:37)]
-      varMat <- varMat[,c(1:17,21:37)]
     }
     if(forecast_weeks[n] == 3){
       vardat <- vardat[,c(1:18,21:38)]
-      varMat <- varMat[,c(1:18,21:38)]
     }
     if(forecast_weeks[n] == 2){
       vardat <- vardat[,c(1:19,21:39)]
-      varMat <- varMat[,c(1:19,21:39)]
     }
 
     #1. RMSE of log(totalperL)
@@ -149,7 +173,7 @@ for (n in 1:length(forecast_weeks)){
     hoa[1,2] <- round(RMSE,2)
 
     #2. predictive variance of log(totalperL)
-    pred_var <- mean(varMat[nrow(varMat),])
+    pred_var <- mean(apply(vardat,2,var))
     hoa[1,3] <- round(pred_var,2)
 
     #3. coverage (% of values falling within 95% predictive interval)
@@ -190,7 +214,7 @@ for (n in 1:length(forecast_weeks)){
   hoa <- data.frame(hoa)
   colnames(hoa) <- c("model_name","RMSE","pred_var","coverage","peak_timing","mean_quantile",
                      "max_quantile","Pearsons_r","mean_bias","max_bias","mean_range")
-  write.csv(hoa,file=file.path(paste("./7_Model_ensemble/",paste0('hindcast_output_analysis_wk_',forecast_weeks[n],'.csv'),sep = "")),row.names = FALSE)
+  write.csv(hoa,file=file.path(paste("./6_Output_analysis/",paste0('hindcast_output_analysis_wk_',forecast_weeks[n],'.csv'),sep = "")),row.names = FALSE)
 }
 
 rm(list = ls())
@@ -206,7 +230,7 @@ rm(list = ls())
 
 
 
-#6. Calculate uncertainty partitioning analysis metrics from relative varmat
+#7. Calculate uncertainty partitioning analysis metrics from relative varmat
 
 #setting up counters and vectors for for-loop
 forecast_weeks <- c(1:4)
