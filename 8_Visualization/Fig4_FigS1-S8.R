a47b5c63677790b151ff6b8bd2778ffaa90b8638
melofton <melofton@vt.edu>
2020-05-22 18:02:29 -0400

    editing Fig. 4

--- 8_Visualization/Fig4_FigS1-S8.R
+++ 8_Visualization/Fig4_FigS1-S8.R
@@ -2,7 +2,7 @@
 #History: created by MEL on 25APR20
 
 #load packages
-pacman::p_load(tidyverse, lubridate, lemon)
+pacman::p_load(tidyverse, lubridate, lemon, grid, gridExtra, cowplot)
 
 #set local directory for writing plots
 my_directory <- "C:/Users/Mary Lofton/Dropbox/Ch5/Gloeo_env_covariate_timeseries/"
@@ -35,33 +35,97 @@ par <- read_csv("./00_Data_files/Covariate_analysis_data/par_daily_summary.csv")
 wind_data <- read_csv("./00_Data_files/Covariate_analysis_data/wind_data.csv")
 
 #join all covariate data with gloeo
-covariates_all <- bind_cols(hc_gloeo_data[,c(1:5,10)], water_temp_data[,-1], schmidt_stability_data[,-1], precip_data[,-1], gdd[,3], swrad[,-1], par[,-1], wind_data[-1]) %>%
-  select(date, year, ln_totalperL, HCS.tempC_min, HCS.tempC_min_lag, ma_7, schmidt.stability_median_diff,
+covariates_all <- bind_cols(hc_gloeo_data[,c(1:10)], water_temp_data[,-1], schmidt_stability_data[,-1], precip_data[,-1], gdd[,3], swrad[,-1], par[,-1], wind_data[-1]) %>%
+  select(date, year, totalperL, HCS.tempC_min, HCS.tempC_min_lag, ma_7, schmidt.stability_median_diff,
          schmidt.stability_max_lag, gdd_sum, precip_mm, AveWindDir_cove_mean_2daylag)
 
 #write plot theme
-mytheme <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+mytheme1 <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                  panel.background = element_blank(), axis.line = element_line(colour = "black"),
                  legend.key = element_blank(),legend.background = element_blank(),
-                 text = element_text(size=16), axis.text.y = element_text(size = 16),
-                 axis.text.x = element_text(size = 12),
+                 text = element_text(size=16), axis.text.y = element_text(size = 14),
                  panel.border = element_rect(colour = "black", fill = NA),
-                 strip.text.x = element_text(face = "bold"))
+                 strip.text.x = element_text(face = "bold"),
+                 strip.background.x = element_blank(),
+                 axis.text.x = element_blank(),
+                 plot.margin = unit(c(0, 1, 0, 0), "cm"),
+                 panel.spacing = unit(-1, "lines"))
 
-#gloeo
-my_y_title <- expression(paste("Log ", italic("G. echinulata"), " total colonies",~~L^-1))
+first4 <- covariates_all %>%
+  filter(year %in% c(2009:2012))
 
-gloeo <- ggplot(data = covariates_all, aes(x = date, y = ln_totalperL))+
-  facet_rep_wrap(vars(year), nrow = 2, ncol = 4, scales = "free_x")+
-  ylab(my_y_title)+
-  theme(strip.background.x = element_blank())+
+gloeo1 <- ggplot(data = first4, aes(x = date, y = totalperL))+
+  facet_rep_wrap(vars(year), nrow = 1, ncol = 4, scales = "free_x")+
+  ylab("")+
   xlab("")+
-  mytheme+
+  ylim(0,82)+
+  mytheme1+
+  geom_point(colour = "chartreuse3")+
+  geom_line(size = 1, colour = "chartreuse3")
+gloeo1
+
+mytheme2 <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                  panel.background = element_blank(), axis.line = element_line(colour = "black"),
+                  legend.key = element_blank(),legend.background = element_blank(),
+                  text = element_text(size=16), axis.text.y = element_text(size = 12),
+                  axis.text.x = element_text(size = 14),
+                  panel.border = element_rect(colour = "black", fill = NA),
+                  strip.text.x = element_blank(),
+                  strip.background.x = element_blank(),
+                  plot.margin = unit(c(0, 1, 0, 0), "cm"),
+                  panel.spacing = unit(-1, "lines"))
+
+gloeo2 <- ggplot(data = first4, aes(x = date, y = totalperL))+
+  facet_rep_wrap(vars(year), nrow = 1, ncol = 4, scales = "free_x")+
+  #ylab(my_y_title)+
+  xlab("")+
+  ylim(0,5)+
+  ylab("")+
+  mytheme2+
   geom_point(colour = "darkgreen")+
   geom_line(size = 1, colour = "darkgreen")
-gloeo
-ggsave(gloeo, filename = file.path(paste(my_directory,"gloeo_timeseries.tif"),sep = ""),
-       device = "tiff",height = 4, width = 8, units = "in", scale = 1.1)
+gloeo2
+
+second4 <- covariates_all %>%
+  filter(year %in% c(2013:2016))
+
+gloeo3 <- ggplot(data = second4, aes(x = date, y = totalperL))+
+  facet_rep_wrap(vars(year), nrow = 1, ncol = 4, scales = "free_x")+
+  ylab("")+
+  xlab("")+
+  ylim(0,82)+
+  mytheme1+
+  geom_point(colour = "chartreuse3")+
+  geom_line(size = 1, colour = "chartreuse3")
+gloeo3
+
+gloeo4 <- ggplot(data = second4, aes(x = date, y = totalperL))+
+  facet_rep_wrap(vars(year), nrow = 1, ncol = 4, scales = "free_x")+
+  #ylab(my_y_title)+
+  xlab("")+
+  ylim(0,5)+
+  ylab("")+
+  mytheme2+
+  geom_point(colour = "darkgreen")+
+  geom_line(size = 1, colour = "darkgreen")
+gloeo4
+
+plot<-plot_grid(gloeo1,gloeo2,gloeo3,gloeo4, align='v', vjust=1, scale = 1,
+                nrow = 4, ncol = 1,
+                rel_heights = c(1.0, 0.7, 1.0, 0.7),
+                labels = c("a","b","c","d"))
+my_y_title <- expression(paste(italic("G. echinulata"), " total colonies",~~L^-1))
+
+y.grob <- textGrob(my_y_title,
+                   gp=gpar(fontface="bold", col="black", fontsize=15), rot=90)
+
+#add to plot
+
+final_plot <- grid.arrange(arrangeGrob(plot, left = y.grob))
+
+
+ggsave(final_plot, filename = file.path(paste(my_directory,"gloeo_timeseries.tif"),sep = ""),
+       device = "tiff",height = 4, width = 8, units = "in", scale = 1.5)
 
 #mintemp
 mintemp <- ggplot(data = covariates_all, aes(x = date, y = HCS.tempC_min))+
