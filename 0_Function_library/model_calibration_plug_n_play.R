2375ff039d2555eb10dfb1a5177defc97cfed7a0
jabrent <jennie.brentrup@gmail.com>
2022-05-09 16:12:33 -0500

    small edits

--- 0_Function_library/model_calibration_plug_n_play.R
+++ 0_Function_library/model_calibration_plug_n_play.R
@@ -2,8 +2,8 @@
 # History:
 # created JAZ 2019-02-15
 # WB Updates
-# MEL updates for seasonal for-loop 30JUL19
-# MEL updates for final publication 27MAR20
+# MEL updates for seasonal for-loop 30JUL2019
+# MEL updates for final publication 27MAR2020
 
 jags_plug_ins <- function(model_name){
 
@@ -109,7 +109,9 @@ jags_plug_ins <- function(model_name){
   data.Quad_2var <- list(y=cal_data$y, year_no = cal_data$year_no, season_weeks=cal_data$season_weeks,covar1=cal_data$covar1, covar2=cal_data$covar2, week_avg1=cal_data$week_avg1,week_avg2=cal_data$week_avg2, beta.m1=0,  beta.m2=0,beta.m3=0,beta.m4=0,beta.m5=0, beta.v1=0.001, beta.v2=0.001,beta.v3=0.001,beta.v4=0.001,beta.v5=0.001,x_ic=-5,tau_ic = 100,a_proc = 0.001,r_proc = 0.001, a_obs = 15.37, r_obs = 7.84)
   variable.names.Quad_2var <- c("tau_proc", "beta1","beta2", "beta3","beta4","beta5", "tau_obs","tau_C1_proc", "tau_C2_proc")
   variable.namesout.Quad_2var <- c("tau_proc", "beta1", "beta2","beta3","beta4","beta5",  "mu", "tau_obs", "tau_C1_proc", "tau_C2_proc")
-  init.Quad_2var <- list(list(tau_proc=0.001, tau_obs = 0.1,  tau_C1_proc = 0.01,tau_C2_proc = 0.01, beta1=-0.5, beta2=-0.5, beta3=-0.5, beta4=-0.5, beta5=-0.5), list(tau_proc=0.1,  tau_obs = 1,tau_C1_proc = 0.1,tau_C2_proc = 0.1, beta1=0, beta2=0, beta3=0, beta4=0, beta5=0), list(tau_proc=1, tau_obs = 5,tau_C1_proc = 1,tau_C2_proc = 1, beta1=0.5,beta2=0.5, beta3=0.5, beta4=0.5, beta5=0.5))
+
+    init.Quad_2var <- list(list(tau_proc=0.001, tau_obs = 0.1,tau_C1_proc = 0.01,tau_C2_proc = 0.01, beta1=-0.5, beta2=-0.5, beta3=-0.5, beta4=-0.5, beta5=-0.5),list(tau_proc=0.1,tau_obs = 1,tau_C1_proc = 0.1,tau_C2_proc = 0.1, beta1=0, beta2=0, beta3=0, beta4=0, beta5=0),list(tau_proc=1, tau_obs = 5,tau_C1_proc = 1,tau_C2_proc = 1, beta1=0.5,beta2=0.5, beta3=0.5, beta4=0.5, beta5=0.5))
+
   params.Quad_2var <- c("tau_proc","beta1", "beta2", "beta3","beta4","beta5","tau_obs","tau_C1_proc", "tau_C2_proc")
 
   data = eval(parse(text = paste0('data.', model_type)))
